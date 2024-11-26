82fd205ec2f2d3f6544d83ad90b718fa55bd7455
AnwarMulla <anwarmulla1717@gmail.com>
2024-11-26 23:14:12 +0530

    Add project files.

---
+++ QRandBarCode Project/BarandQRCode.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BarandQRCode.aspx.cs" Inherits="QRandBarCode_Project.BarandQRCode" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            QRCode Print:<br />
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Create BarCode" /><asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Create QrCode" />
+            <br />
+            <br />
+            <asp:Image ID="Image1" Width="25%" Height="80px" BorderWidth="0" runat="server" />
+            <br />
+            <br />
+            <asp:PlaceHolder ID="PlaceHolder1" runat="server"></asp:PlaceHolder>
+            <br />
+            <asp:Literal ID="Literal1" runat="server"></asp:Literal>
+        </div>
+    </form>
+</body>
+</html>
---
+++ QRandBarCode Project/BarandQRCode.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using QRCoder;
+
+namespace QRandBarCode_Project
+{
+    public partial class BarandQRCode : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Image1.ImageUrl = "https://barcode.tec-it.com/barcode.ashx?data=" + TextBox1.Text + "&code=Code128&translate-es=om";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            string gerenatedbarcode = TextBox1.Text;
+            System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+            imgBarCode.Height = 150;
+            imgBarCode.Width = 150;
+            using (QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            using (QRCodeData qrCodeData = qrGenerator.CreateQrCode(gerenatedbarcode, QRCodeGenerator.ECCLevel.Q))
+            using (PngByteQRCode qrCode = new PngByteQRCode(qrCodeData))
+            {
+                byte[] qrCodeImage = qrCode.GetGraphic(20);
+
+                imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(qrCodeImage);
+            }
+            PlaceHolder1.Controls.Add(imgBarCode);
+            Literal1.Text = TextBox1.Text;
+        }
+    }
+}
